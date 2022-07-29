f4e775ad3304110ef7ab946f33a9a26874495374
Arthur Fusco <fuscoarthur1@gmail.com>
2022-07-28 22:57:57 -0300

    campos obrigatórios adicionados ao modelo do usuário

--- models/user.model.js
+++ models/user.model.js
@@ -2,9 +2,13 @@ import mongoose from "../db.js";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
-    email: String,
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "username required"],
+  },
+  password: { type: String, required: [true, "password required"] },
+  email: { type: String, unique: true, required: [true, "e-mail required"] },
     verified: Boolean,
     salt: String,
 });
