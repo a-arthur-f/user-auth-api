6d97f5d1d16e25cea95f46973c5785ce9eb31e53
Arthur Fusco <fuscoarthur1@gmail.com>
2022-07-28 22:48:18 -0300

    env: variáveis de ambiente adicionadas a conexão com o banco de dados

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const DB_URI = process.env.DB_URI;
 
-mongoose.connect(DB_URI);
+mongoose.connect(DB_URI, {
+  autoIndex: true,
+});
 
 export default mongoose;
