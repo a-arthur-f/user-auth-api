9201482910f87729fc758d723d3f4cc16b1ca510
Arthur Fusco <fuscoarthur1@gmail.com>
2022-07-28 22:59:45 -0300

    refactor: criptografia de senha movida para dentro do modelo do usuário

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "../db.js";
+import bcrypt from "bcrypt";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -9,10 +10,22 @@ const userSchema = new Schema({
   },
   password: { type: String, required: [true, "password required"] },
   email: { type: String, unique: true, required: [true, "e-mail required"] },
-    verified: Boolean,
-    salt: String,
+  verified: Boolean,
+  salt: String,
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  if (this.password) {
+    try {
+      this.password = await bcrypt.hash(this.password, 12);
+    } catch (e) {
+      next("Falha ao gerar hash do password");
+    }
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
