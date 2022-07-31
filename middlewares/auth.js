7438c9dbcc2849d42e8da1f0301f930085b3d118
Arthur Fusco <fuscoarthur1@gmail.com>
2022-07-31 18:36:39 -0300

    feat: authorization middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export const authorization = (req, res, next) => {
+  const [type, token] = req.get("Authorization").split(" ");
+  if (type !== "Bearer")
+    return res.status(403).json({ errors: ["invalid authorization header"] });
+
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ errors: [err] });
+
+    req.decoded = decoded;
+    next();
+  });
+};
