efa9bbfef3ef38441c72dabcb4ccc1e2f835fed7
Arthur Fusco <fuscoarthur1@gmail.com>
2022-07-31 15:44:51 -0300

    feat: login

---
+++ controllers/login.controllers.js
@@ -0,0 +1,31 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const login = async (req, res, next) => {
+  const { user, password } = req.body;
+
+  try {
+    const findUser = await User.findOne({
+      $or: [{ email: user }, { username: user }],
+    });
+    if (findUser) {
+      const isPassword = await bcrypt.compare(password, findUser.password);
+
+      if (isPassword) {
+        const { username, email } = findUser;
+        jwt.sign({ username, email }, process.env.SECRET, (err, token) => {
+          if (err) return next("JWT creation failed");
+
+          return res.status(200).json({ token });
+        });
+      }
+
+      return next("invalid username or password");
+    }
+
+    return next("invalid username or password");
+  } catch (e) {
+    next("login failed");
+  }
+};
