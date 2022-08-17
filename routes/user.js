2428c246a60e4c237e855c2b284ff6ab9170e0d9
Arthur Fusco <fuscoarthur1@gmail.com>
2022-08-16 21:27:02 -0300

    rename: renomeaçao de funções do controller de usuários

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import userController from '../controllers/user.controllers.js';
+import express from "express";
+import userController from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
-router.get('/user/:id?', userController.get);
-router.post('/user', userController.post);
-router.put('/user/:id', userController.put);
-router.delete('/user/:id', userController.delete);
+router.get("/user/:id?", userController.get);
+router.post("/user", userController.create);
+router.put("/user/:id", userController.update);
+router.delete("/user/:id", userController.delete);
 
 export default router;
